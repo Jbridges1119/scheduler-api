b9d33f9f244eb159622ea331c5e72d64e647cedb
Jeff <jbridges1119@gmail.com>
2022-10-24 08:46:01 -0600

    Remove keepAlive to test if Pool resolved issue.

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ const pg = require("pg");
 const client = new pg.Pool({
   connectionString: process.env.DATABASE_URL || "",
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
-  keepAlive: true,
-  keepAliveInitialDelayMillis: 10000
+  // keepAlive: true,
+  // keepAliveInitialDelayMillis: 10000
 });
 
 client
